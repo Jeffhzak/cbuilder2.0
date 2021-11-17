73791ef5d949fae821a77777f4f8f76c62dc5073
jeffhzak <jeffhzak@hotmail.com>
2021-11-17 11:43:09 +0800

    completed data prefetch code for static site generation

---
+++ components/Race/raceApiGet.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const raceApiGet = async (raceName) => {
+    //! Base Info
+    const URL = "https://www.dnd5eapi.co";
+    const res = await axios.get(`${URL}/api/races/${raceName}`);
+
+    const baseInfo = res.data;
+    // console.log(baseInfo)
+
+    //! Traits Info
+    const traitsInfoArray = [];
+    for (const arrayStep of baseInfo.traits) {
+        const traitsInfoURL = `${URL}${arrayStep.url}`;
+        const traitInfo = await axios.get(traitsInfoURL);
+        traitsInfoArray.push(traitInfo.data);
+    } 
+    console.log(traitsInfoArray)
+
+    baseInfo.traits = traitsInfoArray;
+
+    return baseInfo;
+}
