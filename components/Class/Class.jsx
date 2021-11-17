d8f797b4b56b8751d0aa1919b8f7a906f5d45b63
jeffhzak <jeffhzak@hotmail.com>
2021-11-17 13:43:10 +0800

    forgot to branch to class

--- components/Class/Class.jsx
+++ components/Class/Class.jsx
@@ -1,9 +1,31 @@
+import { Card } from '@mui/material';
 import React from 'react'
+import { useState } from "react"
 
-export const Class = () => {
+export const Class = ({allClassInfo}) => {
+
+    // console.log("class.jsx", allClassInfo);
+    const [playerClass, setPlayerClass] = useState("");
+
+    const renderClassChoice = (allClassInfo) => {
+        
+        const classIndexArray = Object.keys(allClassInfo);
+        const classCardRender = classIndexArray.map((classIndex) => {
+            const thisClassInfo = allClassInfo[classIndex];
+            // console.log(thisClassInfo);
+
+            return (
+                <Card key={thisClassInfo.class.index+"erwrewr"}>
+                    
+                </Card>
+            )
+        })
+    }
     return (
+
         <>
             <h1>Class.jsx</h1>
+            {renderClassChoice(allClassInfo)}
         </>
     )
 }
