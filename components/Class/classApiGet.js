d3ec5128efbc9adcd87278bcaaa380ec0df6cd6c
jeffhzak <jeffhzak@hotmail.com>
2021-11-17 09:39:33 +0800

    improved readability of function

--- components/Class/classApiGet.js
+++ components/Class/classApiGet.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 
 export const classApiGet = async (className) => {
+
     //! Base Info
     const URL = "https://www.dnd5eapi.co";
     const res = await axios.get(`${URL}/api/classes/${className}`);
     // console.log(res.data);
     const BaseInfo = res.data;
+    //! cache spellcasting info
     const spellcasting = res?.data?.spellcasting;
+
     //! Level 1 info
     const levelInfoURL = `${URL}${res.data.class_levels}/1`
     const res2 = await axios.get(levelInfoURL);
     // console.log(res2.data);
     const Level1Info = res2.data;
+
     //! Feature Info
     const featureInfoArray = [];
+  
       for (const arrayStep of res2.data.features) {
         const featureInfoURL = `${URL}${arrayStep.url}`;
         const featureInfo = await axios.get(featureInfoURL);
